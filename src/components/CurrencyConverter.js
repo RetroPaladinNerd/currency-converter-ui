e47973ad49d2446056241ec7483db5ad63c38384
RetroPaladinNerd <metalsouleater@gmail.com>
2025-05-06 03:15:57 +0300

    fix

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -117,8 +117,8 @@ const Footer = styled(Box)(({ theme }) => ({
     width: '100vw',
     marginTop: 'auto',
     boxSizing: 'border-box',
-    marginLeft: 'calc(-50vw + 50%)', // Устанавливаем футер на всю ширину экрана
-    marginRight: 'calc(-50vw + 20%)',
+    marginLeft: 'calc(-30vw + 50%)', // Устанавливаем футер на всю ширину экрана
+    marginRight: 'calc(-30vw + 50%)',
 }));
 
 const FooterLinks = styled(Box)({
